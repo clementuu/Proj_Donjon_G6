6d446bf7e64693885adda2e52d6789e948d8b4da
cleme <cleme@DESKTOP-GLTV60N>
2021-03-26 17:41:48 +0100

    Début de classe client

--- src/JeuDojon/Client.java
+++ src/JeuDojon/Client.java
@@ -1,7 +1,39 @@
 package JeuDojon;
 
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
-	//Bon bah la y a encore tout a faire
+public class Client {
+	
+
+	private static BufferedReader in;
+	private static PrintStream out;
+	
+	public static void main(String args[]) throws UnknownHostException, IOException{
+		
+		Socket socket= new Socket ("127.0.0.1", 6516);
+		
+		System.out.println("Bienvenue");
+		System.out.println("Règles du jeu : 4 déplacements maximum par tout autorisés \nLégende : \nX = Joueur \nM = Monstre \n# = Mur \nP = Potion \n§ = Piège \nCommandes : \nz = haut \ns = bas \nd = droite \nq = gauche \n");
+		
+		in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+		out = new PrintStream(socket.getOutputStream());
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println(in.readLine());
+		
+		for(int i=0;i<10;i++){
+			out.println(scanner.nextLine());
+			System.out.println(in.readLine());
+		}
+		
+		scanner.close();
+		socket.close();
+	}
+	
+	
 	
 }
+	
+
