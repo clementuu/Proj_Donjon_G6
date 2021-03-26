9297e18619266d0f1f650d71b5f247087ee425f8
pacnalfi <arisoy10@mail.ru>
2021-03-26 18:26:20 +0100

    push

--- src/JeuDojon/Serveur.java
+++ src/JeuDojon/Serveur.java
@@ -14,169 +14,37 @@ public class Serveur {
 	
 	public static void main(String[] args) {
 		// Premier message
-		System.out.println("Serveur démarré");
-		ServerSocket s = null;
-		try {
-			
-			//Creation socket serveur
-			s = new ServerSocket(PORT);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		int clientNo=0;
-		while(true){
-			try {
-				//en attente d'une connexion avec un client:  
-				Socket client = s.accept();
-				clientNo++;
-				Personnage X = new Personnage(10,10,'X'); 
-				Donjon d = new Donjon(15,10);
-				Objet mur = new Objet('#');
-				Objet potion = new Objet('P');
-				Objet piege = new Objet('�');
-				Personnage M = new Monstres(5,5,'M'); 
-				d.placerPerso(2,8,X);
-				
-				d.placerPerso(14,9,M);
-				
-				for(int i=1; i<=10; i++) {
-					d.placerObj(i,1,mur);
-				}
-				for(int i=15; i>=12; i--) {
-					d.placerObj(i,1,mur);
-				}
-				for(int i=1; i<=15; i++) {
-					d.placerObj(i,10,mur);
-				}
-				for(int j=2; j<=5; j++) {
-					d.placerObj(13,j,mur);
-				}
-				for(int j=1; j<=10; j++) {
-					d.placerObj(1,j,mur);
-				}
-				for(int j=2; j<=9; j++) {
-					d.placerObj(15,j,mur);
-				}
-				for(int i=11; i<15; i++) {
-					d.placerObj(i,7,mur);
-				}
-				for(int i=8; i<=11; i++) {
-					d.placerObj(i,8,mur);
-				}
-				for(int i=2; i<=5; i++) {
-					d.placerObj(i,7,mur);
-				}
-				for(int i=9; i<=12; i++) {
-					d.placerObj(i,3,mur);
-				}
-				for(int j=2; j<=5; j++) {
-					d.placerObj(6,j,mur);
-				}
-				for(int i=3; i<=4; i++) {
-					d.placerObj(i,3,mur);
-				}
-				for(int i=7; i<=11; i++) {
-					d.placerObj(i,5,mur);
-				}
-				
-				d.placerObj(2,7,mur);
-				d.placerObj(3,7,mur);
-				d.placerObj(12,2,piege);
-				d.placerObj(6,9,potion);
-				d.placerObj(10,6,piege);
-				d.placerObj(11,2,piege);
-				d.placerObj(5,3,potion);
-				d.placerObj(2,5,mur);
-				d.placerObj(2,4,mur);
-				d.placerObj(4,6,mur);
-				d.placerObj(12,8,piege);
-				d.placerObj(3,6,piege);
-				d.placerObj(14,4,potion);
-				
-				System.out.println("R�gles du jeu : 4 d�placements maximum par tout autoris�s \nL�gende : \nX = Joueur \nM = Monstre \n# = Mur \nP = Potion \n� = Pi�ge \nCommandes : \nz = haut \ns = bas \nd = droite \nq = gauche \n");
-				
-				d.afficher();
-				
-				//System.out.println( "Test getter getCase() : " + d.getCase(0,0) + "\nDone\n");
-				
-				boolean b=false;
-				
-				for (int manche=0;manche<=10;manche++) {
+		// Premier message
+				System.out.println("Serveur démarré");
+				ServerSocket s = null;
+				try {
 					
-					if(b==false) {
+					//Creation socket serveur
+					s = new ServerSocket(PORT);
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
+				int clientNo=0;
+				while(true){
+					try {
+						//en attente d'une connexion avec un client:  
+						Socket client = s.accept();
+						clientNo++;
 						
-						System.out.println("Veuillez communiquer vos 4 prochains d�placements : ");
-						Scanner sc = new Scanner(System.in);
-						String com = sc.next();
-						System.out.println("Vous avez tap� : " + com + "\n");
-						
-						if(com.length() <= 4 ) {
-							for(int j=0;j<=com.length()-1;j++) {
-								if(com.charAt(j) != 'q' && com.charAt(j) != 'd' && com.charAt(j) != 's' && com.charAt(j) != 'z') {
-									System.err.print("Mauvaise commande, veuillez r��ssayer \n");
-								}
-							}
-							
-							for(int i=0;i<=com.length()-1;i++) {
-								if(com.charAt(i) == 'z') {
-									d.move(X,"Up");
-								}
-								if(com.charAt(i) == 's') {
-									d.move(X,"Down");
-								}
-								if(com.charAt(i) == 'd') {
-									d.move(X,"Right");
-								}
-								if(com.charAt(i) == 'q') {
-									d.move(X,"Left");
-								}
-								
-							if(b==false) {
-								for(int x=0;x<d.getLongueur()-1;x++) {
-									for(int y=0;y<d.getLargeur()-1;y++) {
-										if(d.getCase(0, y)==X.getPerso() || d.getCase(d.getLongueur()-1, y)==X.getPerso() || d.getCase(x, 0)==X.getPerso() || d.getCase(x, d.getLargeur()-1)==X.getPerso()){
-											b=true;	
-										}
-									}
-								}
-							}
-							
-							if(b==true) {
-								
-								System.out.println("F�licitations! Niveau termin� \n");				
-								
-							}
-							
-						}
-												
-						}
-						
-						else System.err.print("Contentez vous de 4 d�placements maximum par tour \n");
-				
-						d.afficher();
-						
+						//création du nouveau thread en passant le socket comme parametre
+						Personnage personnage = new Personnage(10,10,'X');
+						//démarrage 
+						game.start();
+						System.out.println("client "+clientNo+" servi");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-							
+					
 				}
 				
-				/*d.moveUp(X);
-				d.afficher();
-				d.moveLeft(X);
-				d.moveLeft(X);
-				d.afficher();
-				d.moveDown(X);
-				d.afficher();
-				d.moveRight(X); */
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				
 			}
-			
-		}
-		
-		
-	}
 
-}
+		}
--- src/JeuDojon/ServeurPersonnage.java
+++ src/JeuDojon/ServeurPersonnage.java
@@ -2,6 +2,7 @@ package JeuDojon;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class ServeurPersonnage extends Thread{
 		
@@ -17,43 +18,156 @@ public class ServeurPersonnage extends Thread{
 		
 		public void run(){
 			try {
-					in = new BufferedReader(
-				          new InputStreamReader(socket.getInputStream())
-				          );
-					
-					out = new PrintStream(socket.getOutputStream());
-					
-					//envoi du premier message
-					out.println("SERVEUR : en attente de requête");
-					
-					
-					//5 requêtes
-				for(int req=0;req<5;req++){
-					
+				Personnage X = new Personnage(10,10,'X'); 
+				Donjon d = new Donjon(15,10);
+				Objet mur = new Objet('#');
+				Objet potion = new Objet('P');
+				Objet piege = new Objet('�');
+				Personnage M = new Monstres(5,5,'M'); 
+				d.placerPerso(2,8,X);
 				
-					String message = in.readLine();
+				d.placerPerso(14,9,M);
 				
-					if (message.equals("z")){
-						
-						out.println(System.nanoTime());//+" "+req);
-				
-					}
-					else
-					{  
-						
-						out.println("bug");//+req);
-					}
+				for(int i=1; i<=10; i++) {
+					d.placerObj(i,1,mur);
 				}
-				socket.close();
+				for(int i=15; i>=12; i--) {
+					d.placerObj(i,1,mur);
+				}
+				for(int i=1; i<=15; i++) {
+					d.placerObj(i,10,mur);
+				}
+				for(int j=2; j<=5; j++) {
+					d.placerObj(13,j,mur);
+				}
+				for(int j=1; j<=10; j++) {
+					d.placerObj(1,j,mur);
+				}
+				for(int j=2; j<=9; j++) {
+					d.placerObj(15,j,mur);
+				}
+				for(int i=11; i<15; i++) {
+					d.placerObj(i,7,mur);
+				}
+				for(int i=8; i<=11; i++) {
+					d.placerObj(i,8,mur);
+				}
+				for(int i=2; i<=5; i++) {
+					d.placerObj(i,7,mur);
+				}
+				for(int i=9; i<=12; i++) {
+					d.placerObj(i,3,mur);
+				}
+				for(int j=2; j<=5; j++) {
+					d.placerObj(6,j,mur);
+				}
+				for(int i=3; i<=4; i++) {
+					d.placerObj(i,3,mur);
+				}
+				for(int i=7; i<=11; i++) {
+					d.placerObj(i,5,mur);
+				}
 				
+				d.placerObj(2,7,mur);
+				d.placerObj(3,7,mur);
+				d.placerObj(12,2,piege);
+				d.placerObj(6,9,potion);
+				d.placerObj(10,6,piege);
+				d.placerObj(11,2,piege);
+				d.placerObj(5,3,potion);
+				d.placerObj(2,5,mur);
+				d.placerObj(2,4,mur);
+				d.placerObj(4,6,mur);
+				d.placerObj(12,8,piege);
+				d.placerObj(3,6,piege);
+				d.placerObj(14,4,potion);
 				
+				System.out.println("R�gles du jeu : 4 d�placements maximum par tout autoris�s \nL�gende : \nX = Joueur \nM = Monstre \n# = Mur \nP = Potion \n� = Pi�ge \nCommandes : \nz = haut \ns = bas \nd = droite \nq = gauche \n");
 				
+				d.afficher();
 				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				//System.out.println( "Test getter getCase() : " + d.getCase(0,0) + "\nDone\n");
+				
+				boolean b=false;
+				
+				for (int manche=0;manche<=10;manche++) {
+					
+					if(b==false) {
+						
+						System.out.println("Veuillez communiquer vos 4 prochains d�placements : ");
+						Scanner sc = new Scanner(System.in);
+						String com = sc.next();
+						System.out.println("Vous avez tap� : " + com + "\n");
+						
+						if(com.length() <= 4 ) {
+							for(int j=0;j<=com.length()-1;j++) {
+								if(com.charAt(j) != 'q' && com.charAt(j) != 'd' && com.charAt(j) != 's' && com.charAt(j) != 'z') {
+									System.err.print("Mauvaise commande, veuillez r��ssayer \n");
+								}
+							}
+							
+							for(int i=0;i<=com.length()-1;i++) {
+								if(com.charAt(i) == 'z') {
+									d.move(X,"Up");
+								}
+								if(com.charAt(i) == 's') {
+									d.move(X,"Down");
+								}
+								if(com.charAt(i) == 'd') {
+									d.move(X,"Right");
+								}
+								if(com.charAt(i) == 'q') {
+									d.move(X,"Left");
+								}
+								
+							if(b==false) {
+								for(int x=0;x<d.getLongueur()-1;x++) {
+									for(int y=0;y<d.getLargeur()-1;y++) {
+										if(d.getCase(0, y)==X.getPerso() || d.getCase(d.getLongueur()-1, y)==X.getPerso() || d.getCase(x, 0)==X.getPerso() || d.getCase(x, d.getLargeur()-1)==X.getPerso()){
+											b=true;	
+										}
+									}
+								}
+							}
+							
+							if(b==true) {
+								
+								System.out.println("F�licitations! Niveau termin� \n");				
+								
+							}
+							
+						}
+												
+						}
+						
+						else System.err.print("Contentez vous de 4 d�placements maximum par tour \n");
+				
+						d.afficher();
+						
+					}
+							
+				}
+				
+				/*d.moveUp(X);
+				d.afficher();
+				d.moveLeft(X);
+				d.moveLeft(X);
+				d.afficher();
+				d.moveDown(X);
+				d.afficher();
+				d.moveRight(X); */
+			
+			socket.close();
+			
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-	}
-
+					
+				
+   }		
+				
+}
