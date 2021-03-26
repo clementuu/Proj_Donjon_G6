fd0f9c631dedba75a4f804086b1bc13efeb2dae5
cleme <cleme@DESKTOP-GLTV60N>
2021-03-26 17:49:57 +0100

    client

--- src/JeuDojon/Client.java
+++ src/JeuDojon/Client.java
@@ -23,7 +23,7 @@ public class Client {
 		
 		System.out.println(in.readLine());
 		
-		for(int i=0;i<10;i++){
+		for(int i=0;i<5;i++){
 			out.println(scanner.nextLine());
 			System.out.println(in.readLine());
 		}
